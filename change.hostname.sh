4263e87373e5d4dd6d14e70d30bc21b48975015d
marshki <marshki@users.noreply.github.com>
2017-11-06 17:50:15 -0500

    Replace echo with printf
    printf is more portable than echo

--- change.hostname.sh
+++ change.hostname.sh
@@ -6,10 +6,10 @@
 hostn=$(cat /etc/hostname)
 
 #Display existing hostname
-echo "Existing hostname is $hostn"
+printf "%s\n" "Existing hostname is $hostn"
 
 #Ask for new hostname $newhost
-echo "Enter new hostname: "
+printf "%s\n" "Enter new hostname: "
 read newhost
 
 #Change hostname in /etc/hosts & /etc/hostname
@@ -17,7 +17,7 @@ sed -i "s/$hostn/$newhost/g" /etc/hosts
 sed -i "s/$hostn/$newhost/g" /etc/hostname
 
 #Display new hostname
-echo "Your new hostname is $newhost"
+printf "%s\n" "Your new hostname is $newhost"
 
 #Press a key to reboot
 read -s -n 1 -p "Press any key to reboot"
