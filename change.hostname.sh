50b4bb692ce2607418c2b80d7768c2476bcce2dd
marshki <marshki@users.noreply.github.com>
2017-11-07 19:02:04 -0500

    chanhe hostn to currenthost
    Update variable name.

--- change.hostname.sh
+++ change.hostname.sh
@@ -3,18 +3,18 @@
 #Must be run as root. 
 
 #Assign existing hostname to $hostn
-hostn=$(cat /etc/hostname)
+currenthost=$(cat /etc/hostname)
 
 #Display existing hostname
-printf "%s\n" "Existing hostname is $hostn"
+printf "%s\n" "Existing hostname is $currenthost"
 
 #Ask for new hostname $newhost
 printf "%s\n" "Enter new hostname: "
 read newhost
 
 #Change hostname in /etc/hosts & /etc/hostname
-sed -i "s/$hostn/$newhost/g" /etc/hosts
-sed -i "s/$hostn/$newhost/g" /etc/hostname
+sed -i "s/$currenthost/$newhost/g" /etc/hosts
+sed -i "s/$currenthost/$newhost/g" /etc/hostname
 
 #Display new hostname
 printf "%s\n" "Your new hostname is $newhost"
