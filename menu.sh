8faef6b096204ffb419f3e713771584e5f1c42b3
marshki <marshall@morethantomorrow.org>
2017-11-12 11:55:42 -0500

    variable assignments

--- menu.sh
+++ menu.sh
@@ -1,4 +1,11 @@
+#!/bin/bash 
+# Changes hostname in Ubuntu.  
+
+# Assign name of script
 scriptname=`basename "$0"`
+
+# Assign exiting hostname
 currenthost=$(cat /etc/hostname)
 
+# Whiptail message box 
 whiptail --msgbox --backtitle "$scriptname" "The current hostname is $currenthost" 10 40
