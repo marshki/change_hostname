80f71b6dab96e3e6fa188305808bf9fd74456509
marshki <marshall@morethantomorrow.org>
2017-11-12 13:18:50 -0500

    add user input function

--- menu.sh
+++ menu.sh
@@ -1,11 +1,34 @@
 #!/bin/bash 
 # Changes hostname in Ubuntu.  
 
-# Assign name of script
-scriptname=`basename "$0"`
+#Globals 
+script=`basename "$0"`              # Assign name of script
+root_uid=0                          # Assign 0 to root_uid 
+currenthost=$(cat /etc/hostname)    # Assign exiting hostname 
 
-# Assign exiting hostname
-currenthost=$(cat /etc/hostname)
+# Exit if not root
+if [ "$UID" -ne "$root_uid" ]; then 
+    whiptail --backtitle "$script" --msgbox "ROOT privileges are required to continue. Exiting." 10 40
+    exit 1
+fi 
 
-# Whiptail message box 
-whiptail --msgbox --backtitle "$scriptname" "The current hostname is $currenthost" 10 40
+# Display existing hostname 
+whiptail --backtitle "$script" --msgbox "The current hostname is $currenthost." 10 40
+
+# Ask for new hostname 
+newhost=$(whiptail --backtitle "$script" --inputbox "Enter new hostname:" 10 40 3>&1 1>&2 2>&3)
+                                                            
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    read $newhost 
+    printf "$s\n" "User entered: $newhost" 
+else
+    printf "$s\n" "Canceling..."
+    exit 1 
+fi
+
+#change hostname in /etc/hosts & /etc/hostname 
+
+# Display new hostname 
+
+# Press a key to reboot 
