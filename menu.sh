6fb4f71d5f5189eeac0ff6c8722a5fedf385cbad
marshki <marshall@morethantomorrow.org>
2017-11-13 10:49:35 -0500

    update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,31 +1,32 @@
-#!/bin/bash 
+#!/bin/bash
 # Changes hostname in Ubuntu.  
 
-#Globals 
+# Assign Variables 
 script=`basename "$0"`              # Assign name of script
+program="Change Hostname"    	    # Assign title 
 root_uid=0                          # Assign 0 to root_uid 
-currenthost=$(cat /etc/hostname)    # Assign exiting hostname 
+currenthost=$(cat /etc/hostname)    # Assign existing hostname 
 
 # Exit if not root
 if [ "$UID" -ne "$root_uid" ]; then 
-    whiptail --backtitle "$script" --msgbox "ROOT privileges are required to continue. Exiting." 10 40
+    whiptail --backtitle "$script" --title "$program" --msgbox "ROOT privileges are required to continue. Exiting." 10 40
     exit 1
 fi 
 
 # Display existing hostname 
-whiptail --backtitle "$script" --msgbox "The current hostname is $currenthost." 10 40
+whiptail --backtitle "$script" --title "$program" --msgbox "The current hostname is $currenthost." 10 40
 
 # Ask for new hostname 
-newhost=$(whiptail --backtitle "$script" --inputbox "Enter new hostname:" 10 40 3>&1 1>&2 2>&3)
+newhost=$(whiptail --backtitle "$script" --title "$program" --inputbox "Enter new hostname:" 10 40 3>&1 1>&2 2>&3)
                                                             
-exitstatus=$?
-if [ $exitstatus = 0 ]; then
-    read $newhost 
-    printf "$s\n" "User entered: $newhost" 
-else
-    printf "$s\n" "Canceling..."
-    exit 1 
-fi
+#exitstatus=$?
+#if [ $exitstatus = 0 ]; then
+#    read newhost 
+#    printf "$s\n" "User entered: newhost" 
+#else
+#    printf "$s\n" "Canceling..."
+#    exit 1 
+#fi
 
 #change hostname in /etc/hosts & /etc/hostname 
 
