c69223678b542426e40fac1d9fa524d60ddeafcd
marshki <marshall@morethantomorrow.org>
2023-12-28 10:58:09 -0500

    update comments

--- src/macOS/change_hostname.sh
+++ src/macOS/change_hostname.sh
@@ -9,33 +9,33 @@
 # License: MIT
 #
 
-# Assign existing hostname to $hostn
+# Assign existing hostname to $currenthost.
 
 currenthost=$(scutil --get ComputerName)
 
-# Exit if not ROOT. 
+# Exit if not ROOT.
 
 root_user_check () {
-  if [ "$EUID" != "0" ]; then 
+  if [ "$EUID" != "0" ]; then
     printf "%s\n" "ROOT privileges are required to continue. Exiting.">&2
     exit 1
-fi 
+fi
 }
 
-# Display existing hostname
+# Display existing hostname.
 
 show_current_hostname () {
   printf "%s\n" "Existing hostname is: $currenthost"
 }
 
-# Ask for new hostname $newhost
+# Ask for new hostname $newhost.
 
 get_new_hostname () { 
   printf "%s\n" "Enter new hostname: "
   read -r newhost
 } 
 
-# Change hostname in /etc/hosts & /etc/hostname
+# Change hostname via 'scutil'.
 
 change_hostname () {
   scutil --set ComputerName $newhost
@@ -44,29 +44,29 @@ change_hostname () {
   unset $newhost
 } 
 
-# Display new hostname
+# Display new hostname.
 
 show_new_hostname () { 
   printf "%s\n" "Your new hostname is $newhost"
 } 
 
-# Press a key to reboot
+# Press a key to reboot.
 
-rebooty () { 
+rebooty () {
   read -s -n 1 -rp "Press any key to reboot"
-  reboot  
-}  
+  reboot
+}
 
-# Main 
+# Main.
 
 root_user_check
 
-main () { 
-  show_current_hostname 
-  get_new_hostname 
+main () {
+  show_current_hostname
+  get_new_hostname
   change_hostname
   show_new_hostname 
-  rebooty     
-} 
+  rebooty
+}
 
 main "$@"
