63afea4e65aa3b7b4ea60953fb84eee97a10e761
marshki <marshall@morethantomorrow.org>
2025-08-03 13:40:53 -0400

    rm spacing

--- src/GNU_Linux/change_hostname.sh
+++ src/GNU_Linux/change_hostname.sh
@@ -10,11 +10,9 @@
 #
 
 # Assign existing hostname to $hostn
-
 currenthost=$(cat /etc/hostname)
 
 # Exit if not ROOT.
-
 root_user_check () {
   if [ "$EUID" != "0" ]; then
     printf "%s\n" "ROOT privileges are required to continue. Exiting.">&2
@@ -23,40 +21,34 @@ fi
 }
 
 # Display existing hostname
-
 show_current_hostname () {
   printf "%s\n" "Existing hostname is: $currenthost"
 }
 
 # Ask for new hostname $newhost
-
 get_new_hostname () {
   printf "%s\n" "Enter new hostname: "
   read -r newhost
 }
 
 # Change hostname in /etc/hosts & /etc/hostname
-
 change_hostname () {
   sed --in-place "s/$currenthost/$newhost/g" /etc/hosts
   sed --in-place "s/$currenthost/$newhost/g" /etc/hostname
 }
 
 # Display new hostname
-
 show_new_hostname () {
   printf "%s\n" "Your new hostname is: $newhost"
 }
 
 # Press a key to reboot
-
 rebooty () {
   read -s -n 1 -rp "Press any key to reboot"
   reboot
 }
 
 # Main
-
 root_user_check
 
 main () {
