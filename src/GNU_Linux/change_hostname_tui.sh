865e14e9cfacfb60cf51e86574fcdefd956a68cb
marshki <marshall@morethantomorrow.org>
2025-08-03 13:44:38 -0400

    rm excess whitespace

--- src/GNU_Linux/change_hostname_tui.sh
+++ src/GNU_Linux/change_hostname_tui.sh
@@ -8,19 +8,15 @@
 # Author: M. Krinitz <mjk235 [at] nyu [dot] edu>
 # Date: 11-NOV-2017
 # License: MIT
-#
 
 # Assign script name, program name
- 
 script=$(basename "$0")
 program="Change Hostname"
   
 # Assign exisitng hostname
-
 currenthost=$(cat /etc/hostname)
 
 # Exit if not root
-
 root_user_check () {
   if [ "$EUID" != "0" ]; then
   whiptail --backtitle "$script" --title "$program" --msgbox "ROOT privileges are required to continue. Exiting..." 10 40
@@ -29,13 +25,11 @@ fi
 }
 
 # Display existing hostname
-
 show_current_hostname () {
   whiptail --backtitle "$script" --title "$program" --msgbox "The current hostname is: $currenthost." 10 40
 }
 
 # Ask for new hostname $newhost
-
 get_new_hostname () {
   newhost=$(whiptail --backtitle "$script" --title "$program" --inputbox "Enter new hostname:" 10 40 3>&1 1>&2 2>&3)
   
@@ -50,7 +44,6 @@ fi
 }
 
 # Change hostname in /etc/hosts & /etc/hostname
-
 change_hostname () {
   whiptail --backtitle "$script" --title "$program" --msgbox "Changing hostname" 10 40
   
@@ -59,13 +52,11 @@ change_hostname () {
 }
 
 # Display new hostname
-
 show_new_hostname () {
   whiptail --backtitle "$script" --title "$program" --msgbox "Your new hostname is: $newhost" 10 40
 }
 
 # Reboot now or later?
-
 rebooty () {
   if (whiptail --backtitle "$script" --title "$program" --yesno "A reboot is required for changes to take effect. Reboot now?" 10 40) ; then
     reboot
@@ -77,7 +68,6 @@ exit 0
 }
 
 # Main
-
 root_user_check
 
 main () {
